879bc8faf11ed3aa6ffb178112db9930d9562c73
rivaldofez <rivaldofez@gmail.com>
2020-05-17 02:54:54 +0700

    menambah fitur createadmin

--- evestore/src/Screens/SigninScreen.js
+++ evestore/src/Screens/SigninScreen.js
@@ -10,7 +10,6 @@ function SigninScreen(props) {
     const userSignin = useSelector(state=>state.userSignin);
     const {loading,userInfo, error} = userSignin;
     const dispatch = useDispatch();
-
     const redirect = props.location.search?props.location.search.split("=")[1]:'/';
 
     useEffect(() => {
--- evestore_backend/routes/userRoute.js
+++ evestore_backend/routes/userRoute.js
@@ -1,8 +1,30 @@
 import express from 'express';
 import User from '../models/userModel';
-import { getToken } from '../util';
+import { getToken,isAuth } from '../util';
 
 const router = express.Router();
+
+
+router.put('/:id', isAuth, async (req, res) => {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.password = req.body.password || user.password;
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser.id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: getToken(updatedUser)
+      });
+    } else {
+      res.status(404).send({ msg: 'User Not Found' });
+    }
+  
+  });
 
 router.post('/signin', async (req,res) => {
     const signinUser = await User.findOne({
@@ -49,19 +71,17 @@ router.post('/register', async (req,res) => {
 
 router.get("/createadmin", async (req, res) => {
     try {
-        const user = new User({
-            name: 'Rivaldo',
-            email: 'test1@gmail.com',
-            password: '1234',
-            isAdmin: true
-        });
-
-        const newUser = await user.save();
-        res.send(newUser);
+      const user = new User({
+        name: 'Edi',
+        email: 'edi@gmail.com',
+        password: '1234',
+        isAdmin: true
+      });
+      const newUser = await user.save();
+      res.send(newUser);
     } catch (error) {
-        res.send({ msg: error.message });
-
+      res.send({ msg: error.message });
     }
-});
+  });
 
 export default router;
